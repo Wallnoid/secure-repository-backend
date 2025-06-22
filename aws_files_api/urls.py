403dd084ecab5f7bf23c48478d84d25ea625fc9d
Jairo Freire <jairofreireortiz10@gmail.com>
2025-06-22 14:04:38 -0500

    Refactor URL patterns in aws_files_api to improve readability by organizing imports and maintaining consistent formatting.

--- aws_files_api/urls.py
+++ aws_files_api/urls.py
@@ -1,13 +1,13 @@
-
 from django.urls import path
 
-from .views import CreateBucket, FilesView, PrincipalFolder, DownloadFile, FolderCrud
+from .views import (
+    CreateBucket, FilesView, PrincipalFolder, DownloadFile, FolderCrud
+)
 
 urlfilepatterns = [
     path('', FilesView.as_view(), name='get_docs'),
     path('download-file/', DownloadFile.as_view(), name='download_file'),
     path('create-bucket/', CreateBucket.as_view(), name='create_bucket'),
-  
 ]
 
 
